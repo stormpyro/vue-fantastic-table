a8d9bc11d86836266c99e3b5a00a1f6d5e5ea9f8
Stormpyro <tatto1795@gmail.com>
2022-01-18 18:00:22 -0500

    VFT - Populate table

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,24 @@
+<template>
+  <div id="app">
+    <vue-fantastic-table :headers="headers" :rows="rows" />
+  </div>
+</template>
 <script>
-import Vue from 'vue';
-import VueFantasticTable from '@/vue-fantastic-table.vue';
+import Vue from "vue";
+import VueFantasticTable from "@/vue-fantastic-table.vue";
 
 export default Vue.extend({
-  name: 'ServeDev',
+  name: "ServeDev",
   components: {
-    VueFantasticTable
-  }
+    VueFantasticTable,
+  },
+  data: () => ({
+    headers: [{ label: "Test" }, { label: "Animal" }],
+    rows: [
+      { test: "123", animal: "50" },
+      { test: "45", animal: 150 },
+    ],
+  }),
 });
 </script>
 
-<template>
-  <div id="app">
-    <vue-fantastic-table />
-  </div>
-</template>
--- src/vue-fantastic-table.vue
+++ src/vue-fantastic-table.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>
-    <p>Testing</p>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th :key="index" v-for="({ label }, index) in headers">
+          {{ label }}
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr :key="index" v-for="(data, index) in rows">
+        <td
+          :key="index + prop_idx"
+          v-for="(property, prop_idx) in Object.keys(data)"
+        >
+          {{ data[property] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <script>
+import "materialize-css/dist/css/materialize.css";
 export default /*#__PURE__*/ {
   name: "VueFantasticTable", // vue component name
+  props: {
+    rows: {
+      type: Array,
+      required: true,
+    },
+    headers: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
