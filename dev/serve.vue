1869cbf635307cce13cf71f5aba814d373ec8d6a
Stormpyro <tatto1795@gmail.com>
2022-01-18 13:33:51 -0500

    VFT - Base project

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueFantasticTable from '@/vue-fantastic-table.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueFantasticTable
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-fantastic-table />
+  </div>
+</template>
---
+++ src/vue-fantastic-table.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>Testing</p>
+  </div>
+</template>
+<script>
+export default /*#__PURE__*/ {
+  name: "VueFantasticTable", // vue component name
+};
+</script>
